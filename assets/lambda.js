f1fd3f33b93fb4cae1031fd9dd1d3102182fc66e
websta <websta8@gmx.at>
2016-11-24 09:46:37 +0100

    add lambda function

--- assets/lambda.js
+++ assets/lambda.js
@@ -1,2 +1,13 @@
-function lambdaFunction() {
-}
+const doc = require('dynamodb-doc');
+const dynamo = new doc.DynamoDB();
+
+exports.handler = (event, context, callback) => {
+   const done = (err, res) => callback(null, {
+       statusCode: err ? '400' : '200',
+       body: err ? err.message : JSON.stringify(res),
+       headers: {
+           'Content-Type': 'application/json',
+       },
+   });
+   dynamo.scan({ TableName: 'Comment' }, done);
+};
